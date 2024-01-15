b7f78ed2e8a53767e0934ce5e06e880a9006d799
RaulCGVulc <raul@vulcanics.mx>
2024-01-15 07:21:03 -0600

    creando pokecard funcional

--- src/pages/pokedex/components/pokeCard/PokeCard.jsx
+++ src/pages/pokedex/components/pokeCard/PokeCard.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './pokeCard.module.css'
+import { usePokemonStore } from '../../../../store/pokemon'
 
 const PokeCard = ({ pokemon }) => {
+  const fetchPokemon = usePokemonStore(state => state.fetchPokemon)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    fetchPokemon(pokemon.id)
+  }
   return (
-    <section className={styles.container}>
+    <section className={styles.container} onClick={handleClick}>
       <span className={styles.numPoke}># {pokemon.id}</span>
       <div className={styles.imgContainer}>
-        <img src={pokemon?.sprites?.other['official-artwork']?.front_default} alt="Pokemon Image" />
+        <img src={pokemon?.img} alt="Pokemon Image" />
       </div>
       <p className={styles.name}>{pokemon.name}</p>
     </section>
