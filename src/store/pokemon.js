e20dd5d8ea6d52ec37c66af78e568f4fb518f28e
RaulCGVulc <raul@vulcanics.mx>
2024-01-15 07:20:09 -0600

    creando store funcional

--- src/store/pokemon.js
+++ src/store/pokemon.js
@@ -1,10 +1,11 @@
 import { create } from 'zustand'
+import axios from 'axios'
 
 export const usePokemonStore = create((set) => ({
-  pokemon: {
-    name: 'pika'
-  },
+  pokemon: {},
   fetchPokemon: async (id) => {
-    console.log(id)
+    const { data } = await axios.get(`http://localhost:8080/api/v1/pokemon/${id}`)
+    console.log({ data })
+    set({ pokemon: data })
   }
 }))
