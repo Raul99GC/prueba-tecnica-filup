04b6c13ddddfd1a80281fef851385109422e4962
RaulCGVulc <raul@vulcanics.mx>
2024-01-15 00:38:08 -0600

    creando custom hook usePokemons para obtener y almacenar en estado los pokemones

---
+++ src/hooks/usePokemons.js
@@ -0,0 +1,61 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const firstLink = 'http://localhost:8080/api/v1/pokemon?limit=10&page=1'
+
+const usePokemons = () => {
+  const [pokemons, setPokemons] = useState({
+    pokemons: [],
+    nextPage: '',
+    prevPage: '',
+    totalPages: 0,
+    totalCount: 0,
+    link: ''
+  })
+
+  useEffect(() => {
+    axios.get(firstLink)
+      .then(({ data }) => {
+        setPokemons({
+          ...pokemons,
+          pokemons: data.pokemons,
+          nextPage: data?.pagination ? data.pagination.nextPage : '',
+          prevPage: data?.pagination?.prevPage ? data.pagination.prevPage : '',
+          totalPages: data?.pagination ? data?.pagination.totalPages : '',
+          totalCount: data?.totalCount,
+          link: firstLink
+        })
+      })
+  }, [])
+
+  const getPokemons = (link = '') => {
+    setPokemons({
+      pokemons: [],
+      nextPage: '',
+      prevPage: '',
+      totalPages: 0,
+      totalCount: 0,
+      link: ''
+    })
+
+    axios.get(link)
+      .then(({ data }) => {
+        setPokemons({
+          ...pokemons,
+          pokemons: data.pokemons,
+          nextPage: data?.pagination ? data.pagination.nextPage : '',
+          prevPage: data?.pagination?.prevPage ? data.pagination.prevPage : '',
+          totalPages: data?.pagination ? data?.pagination.totalPages : '',
+          totalCount: data?.totalCount,
+          link
+        })
+      })
+  }
+
+  return {
+    ...pokemons,
+    getPokemons
+  }
+}
+
+export default usePokemons
